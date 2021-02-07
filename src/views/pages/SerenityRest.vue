66058a57ddd2982c88521b23fddf55f586bdc25d
LayMui <laymui.toh@taiger.com>
2021-02-07 16:36:23 +0800

    Add practical lab for SerenityRest

--- src/views/pages/SerenityRest.vue
+++ src/views/pages/SerenityRest.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
-    <p class="left">
-      Reference: <a href="https://github.com/LayMui/demo-serenity-rest-screenplay" target="_blank">https://github.com/LayMui/demo-serenity-rest-screenplay
+  <p class="left">
+      Reference: <a href="https://github.com/LayMui/demo-serenity-rest-screenplay" target="_blank">https://github.com/LayMui/demo-serenity-rest-screenplay</a>
+  </p> 
+<p class="left">
+  
+Reference: <a href="https://www.craft.do/s/Ewh3gUHpihSvT8" target="_blank">https://www.craft.do/s/Ewh3gUHpihSvT8
 </a>
-</p> 
+</p>
+
   </div>
 </template>
